0703cfbe5a820cf65f89d5e884dd3244f268bf31
xuxping <18811708041@163.com>
2019-08-10 23:27:59 +0800

    add merge local

--- algorthms/linklist/mergeTwoSortLinklist.c
+++ algorthms/linklist/mergeTwoSortLinklist.c
@@ -1,18 +1,17 @@
 // 合并两个有序链表
+// 1、开辟新的内存
+// 2、不使用新的内存进行合并
 
 #include <stdio.h>
 #include "linklist.h"
 
+/**
+ * 开辟新的内存
+ */ 
 struct LinkNode *mergeTwoSortLinkList(struct LinkNode *head1, struct LinkNode *head2)
 {
-    if (head1 == NULL)
-    {
-        return head2;
-    }
-    if (head2 == NULL)
-    {
-        return head1;
-    }
+    if (head1 == NULL) return head2;
+    if (head2 == NULL) return head1;
 
     struct LinkNode *p1 = head1;
     struct LinkNode *p2 = head2;
@@ -81,6 +80,52 @@ struct LinkNode *mergeTwoSortLinkList(struct LinkNode *head1, struct LinkNode *h
     return newLink;
 }
 
+/**
+ * 不使用新的内存进行合并
+ */ 
+struct LinkNode *mergeTwoSortLinkListByLocal(struct LinkNode *head1, struct LinkNode *head2){
+    if (head1 == NULL) return head2;
+    if (head2 == NULL) return head1;
+
+    struct LinkNode *p1 = head1;
+    struct LinkNode *p2 = head2;
+    struct LinkNode *start = head1;
+    // 确定起始节点
+    if (p1->val > p2->val){
+        start = head2;
+        p2 = start->next;
+    }else{
+        p1 = start->next;
+    }
+
+    struct LinkNode *p3 = start;
+
+    while(p1 != NULL && p2 != NULL){
+        if(p1->val < p2->val){
+            p3->next = p1;
+            p1 = p1->next;
+        }else{
+            p3->next = p2;
+            p2 = p2->next;
+        }
+        p3 = p3->next;
+    }
+
+    while(p1 != NULL){
+        p3->next = p1;
+        p1 = p1->next;
+        p3 = p3->next;
+    }
+
+    while(p2 != NULL){
+        p3->next = p2;
+        p2 = p2->next;
+        p3 = p3->next;
+    }
+
+    return start;
+}
+
 void testMergeTwoSortLinkList()
 {
     // 创建两个链表
@@ -97,6 +142,22 @@ void testMergeTwoSortLinkList()
 
     freeLinkList(head1);
     freeLinkList(head2);
+}
+
+void testMergeTwoSortLinkListLocal()
+{
+    // 创建两个链表
+    int arr1[5] = {1, 3, 4, 6, 9};
+    struct LinkNode *head1 = createLinkList(arr1, 5);
+    printLinkList(head1);
+
+    int arr2[10] = {2, 3, 4, 4, 6, 8, 9, 10, 30, 34};
+    struct LinkNode *head2 = createLinkList(arr2, 10);
+    printLinkList(head2);
+
+    struct LinkNode *newLink = mergeTwoSortLinkListByLocal(head1, head2);
+    printLinkList(newLink);
+
     freeLinkList(newLink);
 }
 
