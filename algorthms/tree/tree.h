d65336971a600f02896029501a1b9e58957254eb
xuxping <xuxp2018@163.com>
2019-08-28 22:48:25 +0800

    add tree.h

---
+++ algorthms/tree/tree.h
@@ -0,0 +1,26 @@
+#ifndef TREE_H_
+#define TREE_H_
+// 树结构定义
+#include <stdlib.h>
+
+typedef struct TreeNode{
+   int val;
+   struct TreeNode *parent; // 根据情况使用
+   struct TreeNode *left;
+   struct TreeNode *right; 
+}TreeNode;
+
+TreeNode *createTreeNode(char *str, int idx, int len);
+TreeNode *createTree(char *str);
+
+// 遍历
+void preOrderWalkRecursion(TreeNode *root);
+void preOrderWalk(TreeNode *root);
+
+void inorderTreeWalkRecursion(TreeNode *root);
+void inOrderWalk(TreeNode *root);
+
+void lastOrderWalkRecursion(TreeNode *root);
+void lastOrderWalk(TreeNode *root);
+
+#endif  // TREE_H_
