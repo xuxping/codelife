0d8dec503dfa011a2d210ca3e241e8d765fe578b
xuxping <xuxp2018@163.com>
2019-08-29 17:26:08 +0800

    add test heap

---
+++ algorthms/tests/test_heap.c
@@ -0,0 +1,41 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../heap.h"
+
+int main(void)
+{
+    struct Heap *h = (struct Heap *)malloc(sizeof(struct Heap));
+    if (h == NULL)
+    {
+        printf("[error] no memory to malloc for heap!");
+        exit(-1);
+    } 
+    h->len = 10;
+    h->heap = (int *)malloc(sizeof(int) * h->len);
+    h->size = 10;
+    for (int i = 0; i < h->len; i++)
+    {
+        h->heap[i] = rand_idx(0, 20);
+    }
+    printf("heap size:%d\n",h->size);
+    display(h->heap, h->size);
+
+    buid_max_heap(h);
+
+    headpsort(h);
+
+    buid_max_heap(h);
+
+    int data1 = pop(h);
+
+    int data2 = pop(h);
+
+    push(h, data1);
+    push(h, data2);
+
+    printf("h->size: %d\n", h->size);
+
+    freeHeap(h);
+    return 0;
+}
